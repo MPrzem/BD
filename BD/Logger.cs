86cbacb8c323138adb115fbaa38016a51dcf6824
MPrzem <przemyslaw.malec1997@gmail.com>
2021-01-27 15:22:08 +0100

    README

--- BD/Logger.cs
+++ BD/Logger.cs
@@ -13,9 +13,7 @@ namespace BD
     {
         private string _serverIp;
         private string _database;
-        /// <summary>
         /// DB User NOT app user
-        /// </summary>
         private string _uid; 
         private string _paswd;
 
@@ -52,7 +50,6 @@ namespace BD
 
         public bool LogintoApp(string _array,string subname, string paswd)
         {
-
             String query = string.Format("SELECT ID FROM {0} WHERE Nazwisko='{1}' and Haslo='{2}'", _array,subname,paswd);
             
             MySqlCommand cmd = new MySqlCommand(query, context);
@@ -60,17 +57,12 @@ namespace BD
             context.Open();
 
             MySqlDataReader reader = cmd.ExecuteReader();
-            /// Ciekawe co bedzie jak tych danych nie odczytam, czy trzeba jakos tego readera czyscic
             if (reader.Read())
             {
                 return true;
             }
             reader.Close();
-
             context.Close();
-
-
-
             return false;
         }
 
---
+++ BD/README.md
@@ -0,0 +1,22 @@
+﻿# Car Factory - Simple database app
+## General info
+App was done as part of the course of database course at studies.  
+It is a system that helps in factory inventory management.
+## Roles in system
+- Warehouse manager-can update quantitty of parts.
+- Designer - can bind parts to subsystems and cars. In other words he makes new models of cars.
+## Weak points
+- No prevention of SQL injection. It is easy to solve by using parameterized queries.
+- Passwords are not hashed. The easiest way to do that is to use MD5 alghorithm.
+## Screens from app
+
+<p align="center">
+  <img src="../ss/Startup.png">  
+  <p align="center" >Startup screen. Role selecting</p>
+  <img src="../ss/Warehouse_manager.png">  
+  <p align="center" >Warehouse manager screen</p>
+  <img src="../ss/Designer.png">
+  <p align="center" >Designer screen</p>
+</p>
+
+
